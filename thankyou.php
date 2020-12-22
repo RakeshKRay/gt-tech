6f2c978ff81d18c04c4d0a4bd382a5703cb54b3f
Rakesh Kymar Ray <rakeshrayk@gmail.com>
2020-12-22 17:08:44 +0530

    22 dec

---
+++ thankyou.php
@@ -0,0 +1,16 @@
+<?php
+
+$name = $_GET['name'];
+
+if ($name == null) {
+    $name = 'guest';
+}
+
+$message = $_GET['message'];
+
+if ($message == null) {
+    $message = 'hello there';
+}
+
+echo "$name says: $message";
+?>
